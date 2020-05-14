beeae5d93642b4d7f109caf4879a466cbf6570c2
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-14 02:17:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.select{|e| e }
+  name_hash.select {|_, v| v == name_hash.values.min}
 
 end
