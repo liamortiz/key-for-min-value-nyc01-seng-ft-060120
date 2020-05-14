1e9755924de472f877d1738402d9be68483e388d
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-14 02:23:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,13 @@
 
 def key_for_min_value(name_hash)
   
-  smallest_value = 0
+  smallest_value = 1000
   key = nil
   
   name_hash.each_pair do |k, v|
-    puts k
     if v < smallest_value
       smallest_value = v
       key = k
-      puts k
     end
   end
   return key
